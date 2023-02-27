202e62ab7ce16de81295414f3ae2d90ab71b1785
Gabriel Chaves Alencar <c.a.gabriel2011@hotmail.com>
2023-02-27 18:53:56 -0300

    1

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+const port = 3000;
+const config = {
+    host     : 'sql9.freemysqlhosting.net',
+    user     : 'sql9601493',
+    password : 'HyWy1H38B3',
+    database : 'sql9601493',
+    port: 3306,
+    ssl: false
+}
+var connection = mysql.createConnection(config);
+connection.connect(function(erro){
+    if(erro){
+        console.log('Erro no my sql: '+erro);
+    }
+    else{
+        console.log('mysql ok');
+       /*
+        connection.query("INSERT INTO leaderboard (Name,Score) VALUES('player2','18')",function(err,results,field){
+            if (err) {
+                console.log('Erro insert: '+erro);
+            }else{
+                console.log('id do insert: '+results.insertId);
+            }
+        });
+        */
+       /*
+       connection.query("UPDATE leaderboard SET Score ='2' WHERE ID =2",function(err,results,field){
+        if (err) {
+            console.log('Erro update: '+erro);
+            connection.rollback();
+        }else{
+            console.log('dados alterados: '+results.changedRows);
+        }
+        
+       });*/
+
+        connection.query('SELECT * FROM leaderboard',function(err,results,field){
+            if (err) {
+                console.log('Erro sql: '+erro);
+                connection.rollback();
+            }else{
+                console.log('select: '+results[0].Score);
+            }
+        });
+        connection.end();
+    }
+}
+);
+app.listen(port,function(){
+    console.log('Servidor escutando na porta: ' + port)
+})
+app.get('/login',function(req,res){
+    console.log("entrando no get/login");
+    var msg_res ={};
+    msg_res.status_code = 200;
+    msg_res.msg_text = "";
+
+    res.status(msg_res.status_code).json(msg_res);
+})
