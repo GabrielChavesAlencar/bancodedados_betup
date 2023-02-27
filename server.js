20d3920c7d88a94609af3f90213b39f80c1f032f
Gabriel Chaves Alencar <c.a.gabriel2011@hotmail.com>
2023-02-27 20:15:25 -0300

    15

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 80;
 const config = {
     host     : 'sql9.freemysqlhosting.net',
     user     : 'sql9601493',
