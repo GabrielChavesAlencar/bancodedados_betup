96eb6b2cc665aa4f9d95ff0adb7857c7f7b14d1d
Gabriel Chaves Alencar <c.a.gabriel2011@hotmail.com>
2023-02-28 19:40:58 -0300

    47

--- index.js
+++ index.js
@@ -106,8 +106,8 @@ app.post('/score',function(req,res){
     msg_res.msg_text2 = "";
 
     
-    msg_res.msg_text = "req: "+req.Name;
-    msg_res.msg_text2 = "res: "+res.Name;
+    msg_res.msg_text = "req: "+req.params + " body: "+req.body;
+    msg_res.msg_text2 = "res: "+res.params+ " body: "+res.body;
     //+req.params.Name+" score: "+req.body.Score + " body: "+ bodyTemp
     res.status(msg_res.status_code).json(msg_res);
     /*
